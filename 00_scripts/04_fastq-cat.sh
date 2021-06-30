9fb917980e0d3e156c2c2be10c9f51ca0d812723
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-30 14:48:40 +1100

    Update 04_fastq-cat.sh

--- 00_scripts/04_fastq-cat.sh
+++ 00_scripts/04_fastq-cat.sh
@@ -19,12 +19,12 @@ cat BRG2V3_R2.paired.fastq.gz BRG2V4_R2.paired.fastq.gz > BRG2_R2_cat.fastq.gz
 cat BRG3V1_R1.paired.fastq.gz BRG3V3_R1.paired.fastq.gz > BRG3_R1_cat.fastq.gz
 cat BRG3V3_R2.paired.fastq.gz BRG3V4_R2.paired.fastq.gz > BRG3_R2_cat.fastq.gz
 
-cat BRG1V1_R1.paired.fastq.gz BRG1V3+_R1.paired.fastq.gz > BRG1+_R1_cat.fastq.gz
-cat BRG1V3_R2.paired.fastq.gz BRG1V4+_R2.paired.fastq.gz > BRG1+_R2_cat.fastq.gz
+#cat BRG1V1_R1.paired.fastq.gz BRG1V3+_R1.paired.fastq.gz > BRG1+_R1_cat.fastq.gz
+#cat BRG1V3_R2.paired.fastq.gz BRG1V4+_R2.paired.fastq.gz > BRG1+_R2_cat.fastq.gz
 
-cat BRG2V1_R1.paired.fastq.gz BRG2V3+_R1.paired.fastq.gz > BRG2+_R1_cat.fastq.gz
-cat BRG2V3_R2.paired.fastq.gz BRG2V4+_R2.paired.fastq.gz > BRG2+_R2_cat.fastq.gz
+#cat BRG2V1_R1.paired.fastq.gz BRG2V3+_R1.paired.fastq.gz > BRG2+_R1_cat.fastq.gz
+#cat BRG2V3_R2.paired.fastq.gz BRG2V4+_R2.paired.fastq.gz > BRG2+_R2_cat.fastq.gz
 
-cat BRG3V1_R1.paired.fastq.gz BRG3V3+_R1.paired.fastq.gz > BRG3+_R1_cat.fastq.gz
-cat BRG3V3_R2.paired.fastq.gz BRG3V4+_R2.paired.fastq.gz > BRG3+_R2_cat.fastq.gz
+#cat BRG3V1_R1.paired.fastq.gz BRG3V3+_R1.paired.fastq.gz > BRG3+_R1_cat.fastq.gz
+#cat BRG3V3_R2.paired.fastq.gz BRG3V4+_R2.paired.fastq.gz > BRG3+_R2_cat.fastq.gz
 
