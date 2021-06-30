63b1853bb87a707098431e1a66ee8ed6a673a7c2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-30 14:35:19 +1100

    Update 01_quality_check_by_FastQC.sh

--- 00_scripts/01_quality_check_by_FastQC.sh
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -5,10 +5,8 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
-#WORKING_DIRECTORY=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/01_raw_data
-#OUTPUT=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/02_quality_check
-WORKING_DIRECTORY=Rhabdastrella_globostellata_microbiome/01_raw_data
-OUTPUT=Rhabdastrella_globostellata_microbiome/02_quality_check
+WORKING_DIRECTORY=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/01_raw_data
+OUTPUT=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/02_quality_check
 
 cd $WORKING_DIRECTORY
 
