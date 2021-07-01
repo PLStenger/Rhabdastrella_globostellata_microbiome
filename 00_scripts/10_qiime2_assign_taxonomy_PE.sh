f9b3d646ce1e7617c7154c59ea1abc6cefca47da
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-07-01 13:40:12 +1100

    Update 10_qiime2_assign_taxonomy_PE.sh

--- 00_scripts/10_qiime2_assign_taxonomy_PE.sh
+++ 00_scripts/10_qiime2_assign_taxonomy_PE.sh
@@ -32,56 +32,74 @@ conda activate qiime2-2019.10
 mkdir -p taxonomy
 mkdir -p export/taxonomy
 
-qiime tools import --type 'FeatureData[Taxonomy]' \
-  --input-format HeaderlessTSVTaxonomyFormat \
-  --input-path /Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/98_database_files/silva_nr99_v138_wSpecies_train_set.fa \
-  --output-path taxonomy/RefTaxo.qza
 
-qiime tools import --type 'FeatureData[Sequence]' \
-  --input-path /Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/98_database_files/silva_nr99_v138_wSpecies_train_set.fa \
-  --output-path taxonomy/DataSeq.qza
+###### All this step was for "old" database, now we uysed new ones 
+######
+######
+######
+######qiime tools import --type 'FeatureData[Taxonomy]' \
+######  --input-format HeaderlessTSVTaxonomyFormat \
+######  --input-path /Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/98_database_files/silva_nr99_v138_wSpecies_train_set.fa \
+######  --output-path taxonomy/RefTaxo.qza
+######
+######qiime tools import --type 'FeatureData[Sequence]' \
+######  --input-path /Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/98_database_files/silva_nr99_v138_wSpecies_train_set.fa \
+######  --output-path taxonomy/DataSeq.qza
+######
+######   
+####### Aim: Extract sequencing-like reads from a reference database.
+####### Warning: For v4 only !!! Not for its2 !!! 
+######
+####### The --p-trunc-len parameter should only be used to trim reference sequences,
+####### if query sequences are trimmed to this same length or shorter.
+###### 
+####### Paired sequences that successfully join will typically be variable in length.
+####### Single reads not truncated at a specific length may also be variable in length.
+######
+####### For classification of paired-end reads and untrimmed single-end reads,
+####### we recommend training a classifier on sequences that have been extracted
+####### at the appropriate primer sites, but are not trimmed !!!
+####### -----
+####### The primer sequences used for extracting reads should be the actual DNA-binding
+####### (i.e., biological) sequence contained within a primer construct.
+######
+####### It should NOT contain any non-biological, non-binding sequence,
+####### e.g., adapter, linker, or barcode sequences.
+######
+####### If you aren't sure what section of your primer sequences are actual DNA-binding
+####### you should consult whoever constructed your sequencing library, your sequencing
+####### center, or the original source literature on these primers.
+######
+####### If your primer sequences are > 30 nt long, they most likely contain some
+####### non-biological sequence !
+######
+######qiime feature-classifier extract-reads --i-sequences taxonomy/DataSeq.qza \
+######        --p-f-primer 'GTGCCAGCMGCCGCGGTAA' \
+######        --p-r-primer 'TCCTCCGCTTATTGATATGC' \
+######        --o-reads taxonomy/RefSeq.qza 
+######        
+######        #--p-trunc-len {params.length} \
+######
+####### Aim: Create a scikit-learn naive_bayes classifier for reads
+######
+######qiime feature-classifier fit-classifier-naive-bayes \
+######  --i-reference-reads taxonomy/RefSeq.qza \
+######  --i-reference-taxonomy taxonomy/RefTaxo.qza \
+######  --o-classifier taxonomy/Classifier.qza
 
-   
-# Aim: Extract sequencing-like reads from a reference database.
-# Warning: For v4 only !!! Not for its2 !!! 
+# With new database :
 
-# The --p-trunc-len parameter should only be used to trim reference sequences,
-# if query sequences are trimmed to this same length or shorter.
- 
-# Paired sequences that successfully join will typically be variable in length.
-# Single reads not truncated at a specific length may also be variable in length.
+# See here for only V4 : https://www.dropbox.com/sh/nz7c5asn6b3hr1j/AADMAR-YZOBkpUQJLumZ9w3wa/ver_0.02?dl=0&subfolder_nav_tracking=1
+# See here for all 16S : https://www.dropbox.com/sh/ibpy9j0clw8dzwm/AAAIVuYnqUzAOxlg2fijePQna/ver_0.02?dl=0&subfolder_nav_tracking=1
 
-# For classification of paired-end reads and untrimmed single-end reads,
-# we recommend training a classifier on sequences that have been extracted
-# at the appropriate primer sites, but are not trimmed !!!
-# -----
-# The primer sequences used for extracting reads should be the actual DNA-binding
-# (i.e., biological) sequence contained within a primer construct.
+# See this thread https://forum.qiime2.org/t/silva-138-classifiers/13131 (found because of this thread : https://forum.qiime2.org/t/silva-138-for-qiime2/12957/4)
 
-# It should NOT contain any non-biological, non-binding sequence,
-# e.g., adapter, linker, or barcode sequences.
 
-# If you aren't sure what section of your primer sequences are actual DNA-binding
-# you should consult whoever constructed your sequencing library, your sequencing
-# center, or the original source literature on these primers.
+cp $DATABASE/SILVA-138-SSURef-full-length-classifier.qza taxonomy/Classifier.qza
+cp $DATABASE/SILVA-138-SSURef-Full-Seqs.qza taxonomy/DataSeq.qza
+cp $DATABASE/Silva-v138-full-length-seq-taxonomy.qza taxonomy/RefTaxo.qza
 
-# If your primer sequences are > 30 nt long, they most likely contain some
-# non-biological sequence !
 
-qiime feature-classifier extract-reads --i-sequences taxonomy/DataSeq.qza \
-        --p-f-primer 'GTGCCAGCMGCCGCGGTAA' \
-        --p-r-primer 'TCCTCCGCTTATTGATATGC' \
-        --o-reads taxonomy/RefSeq.qza 
-        
-        #--p-trunc-len {params.length} \
-        
-
-# Aim: Create a scikit-learn naive_bayes classifier for reads
-
-qiime feature-classifier fit-classifier-naive-bayes \
-  --i-reference-reads taxonomy/RefSeq.qza \
-  --i-reference-taxonomy taxonomy/RefTaxo.qza \
-  --o-classifier taxonomy/Classifier.qza
 
 # Aim: Classify reads by taxon using a fitted classifier
 # --p-reads-per-batch 1000
