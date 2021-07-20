205259ab2a786113b6fc8824630916c59bd60d25
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-07-20 17:32:07 +1100

    Update 07_qiime2_tree_PE.sh

--- 00_scripts/07_qiime2_tree_PE.sh
+++ 00_scripts/07_qiime2_tree_PE.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WORKING_DIRECTORY=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/05_QIIME2
-OUTPUT=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/07_Rhabdastrella_globostellata_microbiome/Rhabdastrella_globostellata_microbiome/05_QIIME2/visual
+WORKING_DIRECTORY=/scratch_vol1/fungi/Rhabdastrella_globostellata_microbiome/05_QIIME2
+OUTPUT=/scratch_vol1/fungi/Rhabdastrella_globostellata_microbiome/05_QIIME2/visual
 
 # Aim: construct a rooted phylogenetic tree
 
@@ -12,35 +12,37 @@ mkdir -p tree
 mkdir -p export/tree
 
 eval "$(conda shell.bash hook)"
-conda activate qiime2-2019.10
+conda activate qiime2-2021.4
 
 #carry out a multiple seqeunce alignment using Mafft
  qiime alignment mafft \
-  --i-sequences core/RepSeq.qza \
+  --i-sequences core/ConRepSeq.qza \
   --o-alignment tree/aligned-RepSeq.qza
 
-#mask (or filter) the alignment to remove positions that are highly variable. These positions are generally considered to add noise to a resulting phylogenetic tree.
+##mask (or filter) the alignment to remove positions that are highly variable. These positions are generally considered to add noise to a resulting phylogenetic tree.
 qiime alignment mask \
   --i-alignment tree/aligned-RepSeq.qza \
   --o-masked-alignment tree/masked-aligned-RepSeq.qza
 
-#create the tree using the Fasttree program
+##create the tree using the Fasttree program
 qiime phylogeny fasttree \
   --i-alignment tree/masked-aligned-RepSeq.qza \
   --o-tree tree/unrooted-tree.qza
 
-#root the tree using the longest root
+##root the tree using the longest root
 qiime phylogeny midpoint-root \
   --i-tree tree/unrooted-tree.qza \
   --o-rooted-tree tree/rooted-tree.qza
   
+  
 #export the tree  
 qiime tools export \
   --input-path tree/unrooted-tree.qza \
-  --output-path tree/
+  --output-path $DATADIRECTORY_V4_bacteria/tree
   
 # This out put is in Newick format, see http://scikit-bio.org/docs/latest/generated/skbio.io.format.newick.html  
 # See it on https://itol.embl.de
+
 
 qiime tools export --input-path tree/unrooted-tree.qza --output-path export/tree/unrooted-tree
 qiime tools export --input-path tree/rooted-tree.qza --output-path export/tree/rooted-tree
